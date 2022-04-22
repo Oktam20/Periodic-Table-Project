82fa0f8c3fe94fd9f0fab0db9745cad7e27629d8
Oktam20 <matejgornal@gmail.com>
2022-04-22 15:14:45 +0000

    feat: added functionality, when atomic number passed to show info

--- element.sh
+++ element.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
+else
+  #WHEN ATOMIC NUMBER IS ACCESSED
+  if [[ $1 =~ [0-9]+ ]]
+  then
+    echo "$($PSQL "SELECT * FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1;")" | while read B B ATOMIC_NUM B SYMBOL B NAME B MASS B MELTING B BOILING B TYPE
+    do
+      echo $ATOMIC_NUM $SYMBOL $NAME $MASS $MELTING $BOILING $TYPE
+      echo "The element with atomic number $ATOMIC_NUM is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+    done
+  fi
 fi
